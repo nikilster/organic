d2a04c4d0fb6c6536e9bde2c44bf38d8b92adca1
Nikil Viswanathan <nikilster@gmail.com>
2015-12-24 23:37:05 -0800

    Initial Commit

---
+++ Organic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Organic
+//
+//  Created by Nikil Viswanathan on 12/24/15.
+//  Copyright © 2015 Nikil Viswanathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Organic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Organic
+//
+//  Created by Nikil Viswanathan on 12/24/15.
+//  Copyright © 2015 Nikil Viswanathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
